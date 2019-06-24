f6a31f02b7d003f95ffda9261614eb4c73e9b7c6
DESKTOP-5CUBI1D\Sheepy <Sheepy@gmx.net>
2019-06-24 21:33:05 +0200

    connection to database via php-script

--- code/anmeldung.php
+++ code/anmeldung.php
@@ -1,3 +1,20 @@
 <?php
 
-echo 'hello world';
+$servername = "localhost";
+$username = "db_admin";
+$password  = "greekApp12345";
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=test",$username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connection successfully!";
+}
+catch(PDOException $e)
+{
+    echo "connection failed; ". $e->getMessage();
+}
+
+$username = $_POST['username'];
+
+
---
+++ code/test.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$username = "db_admin";
+$password  = "greekApp12345";
+
+
+try {
+	$conn = new PDO("mysql:host=$servername;dbname=test",$username,$password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo "Connection successfully!";
+}
+catch(PDOException $e)
+	 {
+	echo "connection failed; ". $e->getMessage();
+}
+
+
