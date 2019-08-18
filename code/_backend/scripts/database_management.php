616b0e904d595b3abc045cf0c690ad58f83cffce
Lukas Mauer <lukasm2010@gmx.de>
2019-08-19 01:34:46 +0200

    fixed the login to database with pdo

--- code/_backend/scripts/database_management.php
+++ code/_backend/scripts/database_management.php
@@ -9,13 +9,13 @@ Skript um den input und output der Datenbank zu regeln
         Database connection with PDO
     */
 
-    $servername = "localhost";
+    $servername = "172.18.0.2"; //ip adress from the container directly not over the bridgeŝ
     $username = "root";
     $password = "test";
     try
     {
         //Create Connection object
-        $conn = new PDO("mysql:host=$servername;dbname=test_work_greek", $username, $password);
+        $conn = new PDO("mysql:host=$servername;port=3306;dbname=test_work_greek", $username, $password);
         //Set the PDO error mode to exception if something goes wrong
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "<h1>Connected sucessfully</h1>";
