f19c0e2e693153c4b8bee4b53904d3843f1c66a0
Lukas Mauer <lukasm2010@gmx.de>
2019-08-19 00:14:51 +0200

    database connection with PDO

--- code/_backend/scripts/database_management.php
+++ code/_backend/scripts/database_management.php
@@ -4,3 +4,26 @@ Skript um den input und output der Datenbank zu regeln
 
 
 <?php
+
+    /*
+        Database connection with PDO
+    */
+
+    $servername = "192.168.0.235";
+    $username = "root";
+    $password = "test";
+    try
+    {
+        //Create Connection object
+        $conn = new PDO("mysql:host=$servername;dbname=test_work_greek");
+        //Set the PDO error mode to exception if something goes wrong
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "<h1>Connected sucessfully</h1>";
+    }
+    catch(PDOException $e)
+    {
+        echo "<h1 color='red'>Connection failed!</h1>";
+    }
+
+    //Close the connection
+    $conn = null;
--- code/_backend/scripts/index.php
+++ code/_backend/scripts/index.php
@@ -1,3 +1,6 @@
+<-- Login page -->
+
+
 <?php
 
 ?>
