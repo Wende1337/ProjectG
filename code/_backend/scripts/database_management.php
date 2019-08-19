605fd4ca2fb44ba8cfc034699b0686d562bc9ed4
Lukas <sheepy@gmx.net>
2019-08-19 16:32:14 +0200

    table management

--- code/_backend/scripts/database_management.php
+++ code/_backend/scripts/database_management.php
@@ -2,26 +2,6 @@
 /*
  * Skript um den input und output der Datenbank zu regeln
  */
-?>
-
-<html>
-    <head>
-        <meta charset="UTF-8">
-
-        <style>
-            #tab_content, th, td{
-                width: 100%;
-                border: 1px solid black;
-                border-collapse: collapse;
-            }
-            th, td{
-                padding: 5px;
-                text-align: left;
-            }
-        </style>
-    </head>
-</html>
-<?php
 
     /*
         Database connection with PDO
@@ -30,64 +10,51 @@
     $servername = "172.18.0.2"; //ip adress from the container directly not over the bridgeŝ
     $username = "root";
     $password = "test";
+
     try
     {
         //Create Connection object
         $conn = new PDO("mysql:host=$servername;port=3306;dbname=test_work_greek;charset=utf8mb4", $username, $password);
         //Set the PDO error mode to exception if something goes wrong
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "<h1>Connected sucessfully</h1>";
-
-        $stmt = $conn->prepare("select id_aufgabe,lektion,uebungstitel,beschreibung,auswahlmoeglichkeiten,am_reihenfolge_relevanz,loesung,loesung_reihenfolge_relevanz,
-                                            max_punkte,schwierigkeitsgrad,schlagworte from Aufgabe");
-        $stmt->execute();
-
-        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        echo "<table id='tab_content'>
-                <tr>
-                    <th> id </th>
-                    <th> lektion </th>
-                    <th> uebungstitel </th>
-                    <th> beschreibung</th>
-                    <th> auswahlmoeglichkeiten </th>
-                    <th> am_reihenfolge_relevanz </th>
-                    <th> loesung </th>
-                    <th> loesung_reihenfolge_relevanz </th>
-                    <th> max_punkte </th>
-                    <th> schwierigkeitsgrad </th>
-                    <th> schlagworte </th>
-                </tr>
-         ";
-        foreach( $stmt->fetchAll() as $array=>$row) {
-            echo "<tr>";
-            foreach ($row as $val=>$columnValue) {
-                echo "<td>".$columnValue."</td>";
-            }
-            echo "</tr>";
-        }
-        echo "<table>";
     }
     catch(PDOException $e)
     {
-        echo "<h1 color='red'>Connection failed!</h1>";
+        echo "Connection failed!".$e->getMessage();
+        //Close the connection
+        $conn = null;
     }
 
-    //Close the connection
-    $conn = null;
+
 
     //Functions for the db communication as interface
         function getAllA1() {
             global $conn;
-            $stmt = $conn->prepare("select id_aufgabe,lektion,uebungstitel,beschreibung,auswahlmoeglichkeiten,am_reihenfolge_relevanz,loesung,loesung_reihenfolge_relevanz,
-                                            max_punkte,schwierigkeitsgrad,schlagworte from Aufgabe");
+            global $stmt;
+            $stmt = $conn->prepare("select Aufgabe.id_aufgabe,lektion,uebungstitel,beschreibung,auswahlmoeglichkeiten,am_reihenfolge_relevanz,loesung,loesung_reihenfolge_relevanz,
+                                            max_punkte,schwierigkeitsgrad,schlagworte from Aufgabe inner join A1 on A1.id_aufgabe = Aufgabe.id_aufgabe");
             $stmt->execute();
 
             $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
             return $result;
         }
         function getAllA2() {
+            global $conn;
+            global $stmt;
+            $stmt = $conn->prepare("select Aufgabe.id_aufgabe,lektion,uebungstitel,beschreibung,auswahlmoeglichkeiten,am_reihenfolge_relevanz,loesung,loesung_reihenfolge_relevanz,
+                                            max_punkte,schwierigkeitsgrad,schlagworte from Aufgabe inner join A2 on A2.id_aufgabe = Aufgabe.id_aufgabe");
+            $stmt->execute();
 
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            return $result;
         }
         function getAllA3() {
+            global $conn;
+            global $stmt;
+            $stmt = $conn->prepare("select Aufgabe.id_aufgabe,lektion,uebungstitel,beschreibung,auswahlmoeglichkeiten,am_reihenfolge_relevanz,loesung,loesung_reihenfolge_relevanz,
+                                            max_punkte,schwierigkeitsgrad,schlagworte from Aufgabe inner join A3 on A3.id_aufgabe = Aufgabe.id_aufgabe");
+            $stmt->execute();
 
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            return $result;
         }
--- code/_backend/scripts/inputmask.php
+++ code/_backend/scripts/inputmask.php
@@ -1,1 +1,72 @@
+<html>
+<html>
+<meta charset="UTF-8">
+
+<style>
+    #tab_content, th, td{
+        width: 100%;
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    th, td{
+        padding: 5px;
+        text-align: left;
+    }
+</style>
+</html>
+<body>
+<form method="POST" action="inputmask.php">
+    <input type="radio" name="table" value="a1" > A1
+    <input type="radio" name="table" value="a2" > A2
+    <input type="radio" name="table" value="a3" > A3 <br>
+    <input type="submit" name="request" value="auswählen">
+</form>
+</body>
+</html>
+
 <?php
+    require 'database_management.php';
+
+    $table = $_POST['table'];
+
+    switch ( $table ) {
+        case "a1" :
+            $result = getAllA1();
+            break;
+        case "a2" :
+            $result = getAllA2();
+            break;
+        case "a3" :
+            $result = getAllA3();
+            break;
+        default:
+            $result = getAllA1();
+    }
+
+    echo "<table id='tab_content'>
+                <tr>
+                    <th> id </th>
+                    <th> lektion </th>
+                    <th> uebungstitel </th>
+                    <th> beschreibung</th>
+                    <th> auswahlmoeglichkeiten </th>
+                    <th> am_reihenfolge_relevanz </th>
+                    <th> loesung </th>
+                    <th> loesung_reihenfolge_relevanz </th>
+                    <th> max_punkte </th>
+                    <th> schwierigkeitsgrad </th>
+                    <th> schlagworte </th>
+                </tr>
+         ";
+    foreach( $stmt->fetchAll() as $array=>$row) {
+        echo "<tr>";
+        foreach ($row as $val=>$columnValue) {
+            echo "<td>".$columnValue."</td>";
+        }
+        echo "</tr>";
+    }
+    echo "<table>";
+    //Close the connection
+    $conn = null;
+?>
+
